3e8d19c6c17c8dea4abcdf3cbd19ea3d0e84dbb1
MohamedElMokhtar <deroua.mohamed.mokhtar@gmail.com>
2025-11-30 13:46:44 +0100

    deleted row

--- models/t_orders.sql
+++ models/t_orders.sql
@@ -1,7 +1,10 @@
 {{
     config(
         materialized='incremental',
-        unique_key='src_id'
+        unique_key='src_id',
+        incremental_strategy='merge',
+        on_schema_change='sync_all_columns',
+        delete_where="_ab_cdc_deleted_at is not null"
     )
 }}
 
@@ -9,13 +12,11 @@ with combined as (
 
     select *, 'DB1' as src
     from {{ source('public', 'db1_orders') }}
-    where _ab_cdc_deleted_at is null
 
     union all
 
     select *, 'DB2' as src
     from {{ source('public', 'db2_orders') }}
-    where _ab_cdc_deleted_at is null
 
 )
 
