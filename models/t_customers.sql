e83aad2d06c2394a33109b4d64e6ab04bcc44f41
MohamedElMokhtar <deroua.mohamed.mokhtar@gmail.com>
2025-11-30 08:48:14 +0100

    first-commit

---
+++ models/t_customers.sql
@@ -0,0 +1,25 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key=['id', 'src']
+    )
+}}
+
+with combined as (
+
+    select *, 'DB1' as src
+    from {{ source('public', 'db1_customers') }}
+
+    union all
+
+    select *, 'DB2' as src
+    from {{ source('public', 'db2_customers') }}
+
+)
+
+select *
+from combined
+{% if is_incremental() %}
+  where _ab_cdc_updated_at >
+      (select max(_ab_cdc_updated_at) from {{ this }})
+{% endif %}
